a54371e7e7385aa69e40913f540587d89b55a2f1
Alana Francino <71753897+AlanaFrancino@users.noreply.github.com>
2022-01-07 13:31:05 -0300

    Insert primeiro user configurações de bd

---
+++ biblioteca/app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+   public function listUser(){
+
+       /* teste ok para insert no bd
+       $user = new User();
+       $user->name = 'Alana Francino';
+       $user->email = 'alana_francino@hotmail.com';
+       $user->password = Hash::make('Alana2806');
+       $user->save();
+       */
+       echo "teste";
+   }
+}
