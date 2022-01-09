1b7c78fd0d9a1a6673a4136faa36115fbe32f4e5
Alana Francino <71753897+AlanaFrancino@users.noreply.github.com>
2022-01-09 10:33:41 -0300

    Install Bootstrap

--- biblioteca/routes/web.php
+++ biblioteca/routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/listagem-usuario', [UserController::class,'listUser'])->name('teste.controller');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
