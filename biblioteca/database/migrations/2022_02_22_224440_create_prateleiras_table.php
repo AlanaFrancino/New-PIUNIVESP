29ec19bbe06a7db5239ee2420ae2e7af239e6a33
VanessaGuarnieri <90491310+VanessaGuarnieri@users.noreply.github.com>
2022-02-22 20:16:37 -0300

    Create tables

---
+++ biblioteca/database/migrations/2022_02_22_224440_create_prateleiras_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParteleirasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prateleiras', function (Blueprint $table) {
+            $table->id();
+            $table->string('rua');
+            $table->string('altura');
+            $table->string('largura');
+            $table->string('descricao');
+            $table->unsignedBigInteger('user_cadastro');
+            $table->boolean('ativo')->default(true);
+            $table->timestamps();
+            $table->foreign('user_cadastro')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parteleiras');
+    }
+}
