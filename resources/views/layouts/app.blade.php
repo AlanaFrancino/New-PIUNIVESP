105d454bc2156ed71c5dbad728689affaa78ba27
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2022-06-29 18:00:14 -0300

    mais font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,6 +69,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,300" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
