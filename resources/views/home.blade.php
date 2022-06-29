1cf501b57f0032f232e383364b5b1ece3b4af385
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2022-06-29 20:27:24 -0300

    sobe graficos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,16 @@
     <div class='dashboard-content'>
         <div class='container'>
             <div class='card'>
+                <br>
                 <div class='card-header'>
                     <h1>Bem vindo ao Web Delmira</h1>
                 </div>
-                <div class='card-body'>
-                    <p>Site em construção</p>
+                <h5 class="card-title" style="display: flex;flex-wrap: nowrap;justify-content: space-around;">Historico de Aquisição e Acervo da biblioteca da UFRN</h5>
+                <div class='card-body' style="display: flex;flex-wrap: nowrap;justify-content: space-around;">
+                    <iframe title="PI2" width="700" height="400"  src="https://app.powerbi.com/view?r=eyJrIjoiMTBkMzBlMzQtNGI1OS00ZTkwLTgyMTEtMjRmZjk4ZjM0NWEwIiwidCI6ImQ0NDU2M2FhLTIxZTMtNDNkMC1iMDlkLTU0MGNjNjlhMzlkNiIsImMiOjF9&pageName=ReportSectiona97cc6c0de8864a55ad2" frameborder="0" allowFullScreen="true"></iframe>
                 </div>
             </div>
         </div>
 
-        <iframe title="pi_controle_estoque" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYjZiMzY5MzItOWU5Ny00ZTQwLTlmOGEtNWUxM2EwMTkwMzNlIiwidCI6ImQ0NDU2M2FhLTIxZTMtNDNkMC1iMDlkLTU0MGNjNjlhMzlkNiIsImMiOjF9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
     </div>
 @endsection
