4f928ea7c6215e0089bcced3666aac157a892b0a
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2022-11-14 15:01:57 -0300

    Update api.blade.php

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -1,4 +1,4 @@
 <?php
-file_put_contents('http://webdelmira2022.herokuapp.com/resources/views/UIDContainer.blade.php'),$Write) 
+file_put_contents("http://webdelmira2022.herokuapp.com".resource_path('views/UIDContainer.blade.php'),$Write) 
 ?>
 {{resource_path('views/UIDContainer.blade.php')}}
