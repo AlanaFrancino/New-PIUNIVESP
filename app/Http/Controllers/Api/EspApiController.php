36dd4a236ed2a8ae615b9d6dfd8f7345473b6bc2
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2022-11-14 15:32:26 -0300

    Update EspApiController.php

--- app/Http/Controllers/Api/EspApiController.php
+++ app/Http/Controllers/Api/EspApiController.php
@@ -9,6 +9,8 @@ class EspApiController extends Controller
 {
     public function search(Request $request)
     {
+        $Write="<?php $" . "UIDresult=''; " . "echo $" . "UIDresult;" . " ?>";
+        file_put_contents(resource_path('views/UIDContainer.blade.php'),$Write);
         $UIDresult = $request->UIDresult;
         $Write="<?php $" . "UIDresult='" . $UIDresult . "'; " . "echo $" . "UIDresult;" . " ?>";
         file_put_contents(resource_path('views/UIDContainer.blade.php'),$Write);
