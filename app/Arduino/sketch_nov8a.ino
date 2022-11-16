fbef38051d8712a7508385c986ad135d0107bf4d
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2022-11-16 09:52:53 -0300

    sobe code

---
+++ app/Arduino/sketch_nov8a.ino
@@ -0,0 +1,195 @@
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#define SS_PIN    D4
+#define RST_PIN   D8 //--> RST is connected to pinout D1
+MFRC522 mfrc522(SS_PIN, RST_PIN);  //--> Create MFRC522 instance.
+
+#define ON_Board_LED 2  //--> Defining an On Board LED, used for indicators when the process of connecting to a wifi router
+
+//----------------------------------------SSID and Password of your WiFi router-------------------------------------------------------------------------------------------------------------//
+const char* ssid = "Alana e Fernando";
+const char* password = "Alana281801";
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+ESP8266WebServer server(80);  //--> Server on port 80
+
+int readsuccess;
+byte readcard[4];
+char str[32] = "";
+String StrUID;
+WiFiClient wifiClient;
+//-----------------------------------------------------------------------------------------------SETUP--------------------------------------------------------------------------------------//
+void setup() {
+  Serial.begin(115200); //--> Initialize serial communications with the PC
+  SPI.begin();      //--> Init SPI bus
+  mfrc522.PCD_Init(); //--> Init MFRC522 card
+
+  delay(500);
+
+  WiFi.begin(ssid, password); //--> Connect to your WiFi router
+  Serial.println("");
+
+  pinMode(ON_Board_LED, OUTPUT);
+  digitalWrite(ON_Board_LED, HIGH); //--> Turn off Led On Board
+
+  //----------------------------------------Wait for connection
+  Serial.print("Connecting");
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Conectando no wifi...");
+
+  uint32_t notConnectedCounter = 0;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    //----------------------------------------Make the On Board Flashing LED on the process of connecting to the wifi router.
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Conectando no wifi...");
+    digitalWrite(ON_Board_LED, LOW);
+    delay(250);
+    digitalWrite(ON_Board_LED, HIGH);
+    delay(250);
+    notConnectedCounter++;
+    if(notConnectedCounter > 50) { // Após 5 segundos nossa placa é resetada
+        Serial.println("Reiniciando placa...");
+        ESP.restart();
+    }
+  }
+  digitalWrite(ON_Board_LED, HIGH); //--> Turn off the On Board LED when it is connected to the wifi router.
+  //----------------------------------------If successfully connected to the wifi router, the IP Address that will be visited is displayed in the serial monitor
+  Serial.println("");
+  Serial.print("Successfully connected to : ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Conectando Wifi...");
+  lcd.setCursor(0,1);
+  lcd.print(WiFi.localIP());
+  SPI.begin();
+
+  Serial.println("Please tag a card or keychain to see the UID !");
+  Serial.println("");
+
+  Serial.println("Aproxime o seu cartao do leitor...");
+  Serial.println();
+  delay(3000);
+  mensageminicial();
+
+
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//-----------------------------------------------------------------------------------------------LOOP---------------------------------------------------------------------------------------//
+void loop() {
+  // put your main code here, to run repeatedly
+  readsuccess = getid();
+
+  if (readsuccess) {
+    digitalWrite(ON_Board_LED, LOW);
+    HTTPClient http;    //Declare object of class HTTPClient
+
+    String UIDresultSend, postData;
+    UIDresultSend = StrUID;
+
+    //Post Data
+    postData = "UIDresult=" + UIDresultSend;
+    String apiGetData = "http://webdelmira2022.herokuapp.com/api/search?"+postData;//Specify request destination
+
+    // Add values in the document
+    //
+    
+    
+    http.begin(wifiClient, apiGetData);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //Specify content-type header
+    
+
+    int httpCode = http.POST(postData);   //Send the request
+    String payload = http.getString();    //Get the response payload
+    Serial.println(apiGetData);
+    Serial.println(UIDresultSend);
+    Serial.println("{\"UIDresult\":\""+UIDresultSend+"\"}");
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+
+    if (httpCode > 0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpCode);
+      String payload = http.getString();
+      Serial.println(payload);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Enviado");
+      delay(3000);
+      mensageminicial();
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpCode);
+      lcd.clear();
+      lcd.setCursor(0,1);
+      lcd.print("Error code: ");
+      lcd.setCursor(0,0);
+      lcd.print(httpCode);
+      delay(3000);
+      mensageminicial();
+    }
+
+    digitalWrite(ON_Board_LED, HIGH);
+  }
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//----------------------------------------Procedure for reading and obtaining a UID from a card or keychain---------------------------------------------------------------------------------//
+int getid() {
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return 0;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return 0;
+  }
+
+
+  Serial.print("THE UID OF THE SCANNED CARD IS : ");
+
+  for (int i = 0; i < 4; i++) {
+    readcard[i] = mfrc522.uid.uidByte[i]; //storing the UID of the tag in readcard
+    array_to_string(readcard, 4, str);
+    StrUID = str;
+  }
+  mfrc522.PICC_HaltA();
+  return 1;
+}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//----------------------------------------Procedure to change the result of reading an array UID into a string------------------------------------------------------------------------------//
+void array_to_string(byte array[], unsigned int len, char buffer[]) {
+  for (unsigned int i = 0; i < len; i++)
+  {
+    byte nib1 = (array[i] >> 4) & 0x0F;
+    byte nib2 = (array[i] >> 0) & 0x0F;
+    buffer[i * 2 + 0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+    buffer[i * 2 + 1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+  }
+  buffer[len * 2] = '\0';
+}
+
+void mensageminicial(){
+  lcd.clear();
+  lcd.print(" Aproxime o seu");  
+  lcd.setCursor(0,1);
+  lcd.print("cartao do leitor");  
+}
