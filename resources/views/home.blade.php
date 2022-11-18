58ec33081f3ac818f8452e05b02132ff49e70382
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2022-11-18 09:18:02 -0300

    sobe home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
                 <div class='card-header'>
                     <h1>Bem vindo ao Web Delmira</h1>
                 </div>
-                <h5 class="card-title" style="display: flex;flex-wrap: nowrap;justify-content: space-around;">Historico de Aquisição e Acervo da biblioteca da UFRN</h5>
+                <h5 class="card-title" style="display: flex;flex-wrap: nowrap;justify-content: space-around;">Fluxo de empréstimos e acervo bibliográfico</h5>
                 <div class='card-body' style="display: flex;flex-wrap: nowrap;justify-content: space-around;">
-                    <iframe title="PI2" width="700" height="400"  src="https://app.powerbi.com/view?r=eyJrIjoiYWU0YWU4NTAtZDJiNS00YzJmLThkNDEtOTA2M2RiZTBjNTU4IiwidCI6ImQ0NDU2M2FhLTIxZTMtNDNkMC1iMDlkLTU0MGNjNjlhMzlkNiIsImMiOjF9" frameborder="0" allowFullScreen="true"></iframe>
+                    <iframe title="PI3" width="700" height="400"  src="https://dataplatform.cloud.ibm.com/dashboards/f636c54b-c076-47fa-9d54-34bf80ece421/view/0028c51a19ed22f472edf6e407ca25527432775fb1bb8b56d1d17b495d682597f06c46c7c87b1e58df135131a2bf105bcf" frameborder="0" allowFullScreen="true"></iframe>
                 </div>
             </div>
         </div>
